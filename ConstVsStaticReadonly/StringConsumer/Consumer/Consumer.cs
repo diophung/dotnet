9355941ef5f331e73d88ced068d46d7efc456c3e
diophung <diophung@outlook.com>
2014-02-26 16:41:17 +0800

    Demo difference between [static readonly] and [const]

---
+++ ConstVsStaticReadonly/StringConsumer/Consumer/Consumer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StringProducer;
+
+namespace StringConsumer
+{
+    public class Consumer
+    {
+	    public static void Main(string[] args)
+	    {
+			string constStr = Producer.ConstString;
+			string staticStr = Producer.StaticReadonlyStringValue;
+
+
+			Console.WriteLine(constStr);
+			Console.WriteLine(staticStr);
+		    Console.ReadLine();
+	    }
+    }
+}
---
+++ ConstVsStaticReadonly/StringProducer/StringProducer/Producer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringProducer
+{
+    public static class Producer
+    {
+		public static readonly string StaticReadonlyStringValue = "StaticReadonlyStringValue = 1";
+		public const string ConstString = "ConstString = 1";
+    }
+}
